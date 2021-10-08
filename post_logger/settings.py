c28f42f71b5ade2beb9903543658994a45bb19e4
Rakesh <rakeshreddyaala@gmail.com>
2021-10-08 16:30:53 +0530

    updated static route

--- post_logger/settings.py
+++ post_logger/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
